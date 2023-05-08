d891820764c3894e062f1a919297aed04294750f
Adewunmi Oladele <rkutalian@gmail.com>
2023-05-08 13:28:44 +0100

    Update settings.py

--- Phoenix_mart/Phoenix/settings.py
+++ Phoenix_mart/Phoenix/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qjgq$5%nz-9eiyo%975-3veidw2x=-#tmwb=o&-0(jyvzh5&vp'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='phoenixmart')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
