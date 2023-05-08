5e8cd0e71b0afca4b50c4432dba98c2edb6b3673
Adewunmi Oladele <rkutalian@gmail.com>
2023-05-08 12:52:41 +0100

    Update settings.py

--- Phoenix_mart/Phoenix/settings.py
+++ Phoenix_mart/Phoenix/settings.py
@@ -35,6 +35,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
